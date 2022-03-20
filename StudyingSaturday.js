2950e3cfc44ee10464b73ac7d6ba98fa5bfcacaa
Derek Andersen <derekcandersen@gmail.com>
2022-03-20 18:05:43 -0400

    prettify js :)

--- StudyingSaturday.js
+++ StudyingSaturday.js
@@ -1,70 +1,77 @@
-const { MessageEmbed } = require('discord.js');
-let fs = require("fs")
+const { MessageEmbed } = require("discord.js");
+let fs = require("fs");
 
-function beginRegistration (derkscord) {
-  console.log('Starting Studying Saturday registration phase...');
+function beginRegistration(derkscord) {
+  console.log("Starting Studying Saturday registration phase...");
 
   // define variable for classroom channel
   const classroom = derkscord.channels.cache.get(classroom_channel_id);
-  let participant_role = derkscord.roles.cache.find(role => role.name === "Participant");
+  let participant_role = derkscord.roles.cache.find(
+    (role) => role.name === "Participant"
+  );
   let participant_role_id = participant_role.id;
 
-  const timerEmbed = new MessageEmbed().setTitle('old title');
+  const timerEmbed = new MessageEmbed().setTitle("old title");
 
-  classroom.send('Registration is now open! React with 🤓 to join today\'s session.').then(sent => { // 'sent' is the message just sent
-    sent.react('🤓');
+  classroom
+    .send("Registration is now open! React with 🤓 to join today's session.")
+    .then((sent) => {
+      // 'sent' is the message just sent
+      sent.react("🤓");
 
-    const filter = (reaction, user) => {
-	     return reaction.emoji.name === '🤓' && user.id !== sent.author.id;
-    };
+      const filter = (reaction, user) => {
+        return reaction.emoji.name === "🤓" && user.id !== sent.author.id;
+      };
 
-    const reactionCollector = sent.createReactionCollector({ filter, time: 5000 }); // 9m 50s
+      const reactionCollector = sent.createReactionCollector({
+        filter,
+        time: 5000,
+      }); // 9m 50s
 
       //classroom.send({ embeds: [timerEmbed] }).then(msg => {
-        //setTimeout(function () {
-          //const timerEmbed = new MessageEmbed(msg).setTitle('new title');
-          //msg.edit({ embeds: [timerEmbed] });
-        //}, 2000)
+      //setTimeout(function () {
+      //const timerEmbed = new MessageEmbed(msg).setTitle('new title');
+      //msg.edit({ embeds: [timerEmbed] });
+      //}, 2000)
       //})
 
-    reactionCollector.on('collect', (reaction, user) => {
-  	   console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
+      reactionCollector.on("collect", (reaction, user) => {
+        console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
 
-       // add Participant role to user
-       let member = derkscord.members.cache.get(user.id);
-       member.roles.add(participant_role_id);
-       classroom.send(`<@${user.id}> joined!`);
+        // add Participant role to user
+        let member = derkscord.members.cache.get(user.id);
+        member.roles.add(participant_role_id);
+        classroom.send(`<@${user.id}> joined!`);
+      });
+
+      reactionCollector.on("end", (collected) => {
+        console.log(`Total collected participants: ${collected.size}`);
+        classroom.send("Registration is now closed.");
+      });
+    })
+    .catch((e) => {
+      console.log(e);
     });
-
-    reactionCollector.on('end', collected => {
-  	   console.log(`Total collected participants: ${collected.size}`);
-       classroom.send('Registration is now closed.');
-    });
-  }).catch(e => {
-    console.log(e);
-  });
-
 } // end of function
 
-function beginStudy (derkscord) {
-  console.log('Starting Studying Saturday event...');
+function beginStudy(derkscord) {
+  console.log("Starting Studying Saturday event...");
 
   // define variable for classroom channel
   const classroom = derkscord.channels.cache.get(classroom_channel_id);
 
   // read topics.txt into a list
-  const readFileLines = filename =>
-      fs.readFileSync(filename)
-      .toString('UTF8')
-      .split('\r\n');
-  const topics = readFileLines('./topics.txt');
+  const readFileLines = (filename) =>
+    fs.readFileSync(filename).toString("UTF8").split("\r\n");
+  const topics = readFileLines("./topics.txt");
   // remove the last item form the list, because it's always an empty string
   topics.splice(-1, 1);
   // choose a random topic
   const x = Math.floor(topics.length * Math.random());
 
-  classroom.send(`Today\'s topic is: **${topics[x]}**!\nYou have 20 minutes to research this topic.\nGood luck! 🤓`);
-
+  classroom.send(
+    `Today\'s topic is: **${topics[x]}**!\nYou have 20 minutes to research this topic.\nGood luck! 🤓`
+  );
 } // end of function
 
 module.exports = { beginRegistration, beginStudy };
--- bot.js
+++ bot.js
@@ -1,166 +1,202 @@
 #!/usr/bin/env node
 
-const Discord = require('discord.js');
-const cron = require('cron');
-const fs = require('fs');
+const Discord = require("discord.js");
+const cron = require("cron");
+const fs = require("fs");
 const config = require("./config.json");
-const basicCommands = require('./commands/basic_commands.json');
-const StudyingSaturday = require('./StudyingSaturday.js');
+const basicCommands = require("./commands/basic_commands.json");
+const StudyingSaturday = require("./StudyingSaturday.js");
 
 var commandList = []; // for storing a list of command names
 const prefix = "$";
 
 const client = new Discord.Client({
   intents: [
-		Discord.Intents.FLAGS.GUILDS,
-		Discord.Intents.FLAGS.GUILD_MEMBERS,
-		Discord.Intents.FLAGS.GUILD_MESSAGES,
-		Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-		Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS ]
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MEMBERS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
 });
 
 // sets a Discord.Collection of bot commands
-function setBotCommands (client) {
-	client.commands = new Discord.Collection();
-	const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const CommandClass = require(`./commands/${file}`);
-		const command = new CommandClass();
-		client.commands.set(command.name, command);
-		commandList.push(command.name);
-	}
-	for (const name in basicCommands) {
-		commandList.push(name);
-	}
+function setBotCommands(client) {
+  client.commands = new Discord.Collection();
+  const commandFiles = fs
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const CommandClass = require(`./commands/${file}`);
+    const command = new CommandClass();
+    client.commands.set(command.name, command);
+    commandList.push(command.name);
+  }
+  for (const name in basicCommands) {
+    commandList.push(name);
+  }
 }
 
 // EVENT LISTENERS //
-client.once('ready', () => {
+client.once("ready", () => {
   console.log(`Ready! Logged in as ${client.user.tag}.`);
 
-	// Set bot's displayed activity on Discord
-	client.user.setActivity('Studying Saturdays');
+  // Set bot's displayed activity on Discord
+  client.user.setActivity("Studying Saturdays");
 
-	// Create collection of nonbasic bot commands
-	setBotCommands(client);
+  // Create collection of nonbasic bot commands
+  setBotCommands(client);
 
-	// Define some relevant IDs
-	derkscord_id = '577602263682646017';
-	classroom_channel_id = '954723619924226129';
-	learner_role_id = '954771555680944270';
+  // Define some relevant IDs
+  derkscord_id = "577602263682646017";
+  classroom_channel_id = "954723619924226129";
+  learner_role_id = "954771555680944270";
 
-
-	const derkscord = client.guilds.cache.get(derkscord_id);
+  const derkscord = client.guilds.cache.get(derkscord_id);
   const classroom = derkscord.channels.cache.get(classroom_channel_id);
 
-	//StudyingSaturday.beginRegistration(derkscord);
+  //StudyingSaturday.beginRegistration(derkscord);
   //StudyingSaturday.beginStudy(derkscord);
 
-	// Weekly reminder for Studying Saturdays event
-	// This runs every Saturday 10 minutes prior to 11:00:00
-	let reminderAnnouncement = new cron.CronJob('5 * * * * 6', () => {
-
-        classroom.send('<@&' + learner_role_id + '> *Studying Saturday* will begin in 10 minutes!');
-        StudyingSaturday.beginRegistration(derkscord);
-  });
-
-	// Weekly Studying Saturdays announcement message
-	// This runs every Saturday at 11:00:00
-	let studyAnnouncement = new cron.CronJob('0 0 11 * * 6', () => {
-         let participant_role = derkscord.roles.cache.find(role => role.name === "Participant");
-         let participant_role_id = participant_role.id;
-         classroom.send('<@&' + participant_role_id + '> It\'s time for *Studying Saturday*!');
-         StudyingSaturday.beginStudy(derkscord);
-  });
-
-  let writingAnnouncement = new cron.CronJob('0 20 11 * * 6', () => {
-    let participant_role = derkscord.roles.cache.find(role => role.name === "Participant");
-    let participant_role_id = participant_role.id;
-         classroom.send('<@&' + participant_role_id + '> Studying Phase has ended! Write a short paragraph about your learning. 10 minutes!');
-  });
-
-  let discussionAnnouncement = new cron.CronJob('0 30 11 * * 6', () => {
-    let participant_role = derkscord.roles.cache.find(role => role.name === "Participant");
-    let participant_role_id = participant_role.id;
-         classroom.send('<@&' + participant_role_id + '> Writing Phase has ended! Discuss the topic with the group to **solidify your learning**.\nThe classroom closes in 30 minutes.');
-  });
-
-  let overAnnouncement = new cron.CronJob('40 * * * * 6', () => {
-         classroom.send('Studying Saturday has ended. See you next week! 🤓');
-         //const role = derkscord.roles.cache.get(participant_role_id);
-         let participant_role = derkscord.roles.cache.find(role => role.name === "Participant");
-
-
-         participant_role.delete('Studying Saturday is over; removed role from all users.')
-         .then(console.log)
-         .catch(console.error);
-  });
-
-  let roleCreationSchedule = new cron.CronJob('15 * * * * *', () => {
+  let roleCreationSchedule = new cron.CronJob("0 0 9 * * 6", () => {
+    // SAT 9 AM
     // creates the Participant role daily
-    derkscord.roles.create({
-       name: "Participant",
-       color: "#0aa22c",
-       position: 99,
-       mentionable: true
-    })
-    .then(console.log)
-    .catch(console.error);
+    derkscord.roles
+      .create({
+        name: "Participant",
+        color: "#0aa22c",
+        position: 99,
+        mentionable: true,
+      })
+      .then(console.log)
+      .catch(console.error);
   });
 
-// Enable the announcements
-reminderAnnouncement.start();
-studyAnnouncement.start();
-writingAnnouncement.start();
-discussionAnnouncement.start();
-overAnnouncement.start();
-roleCreationSchedule.start();
+  // Weekly reminder for Studying Saturdays event
+  // This runs every Saturday 10 minutes prior to 11:00:00
+  let reminderAnnouncement = new cron.CronJob("0 50 10 * * 6", () => {
+    // SAT 10:50 AM
+
+    classroom.send(
+      "<@&" +
+        learner_role_id +
+        "> *Studying Saturday* will begin in 10 minutes!"
+    );
+    StudyingSaturday.beginRegistration(derkscord);
+  });
+
+  // Weekly Studying Saturdays announcement message
+  // This runs every Saturday at 11:00:00
+  let studyAnnouncement = new cron.CronJob("0 0 11 * * 6", () => {
+    // SAT 11 AM
+    let participant_role = derkscord.roles.cache.find(
+      (role) => role.name === "Participant"
+    );
+    let participant_role_id = participant_role.id;
+    classroom.send(
+      "<@&" + participant_role_id + "> It's time for *Studying Saturday*!"
+    );
+    StudyingSaturday.beginStudy(derkscord);
+  });
+
+  let writingAnnouncement = new cron.CronJob("0 20 11 * * 6", () => {
+    // SAT 11:20 AM
+    let participant_role = derkscord.roles.cache.find(
+      (role) => role.name === "Participant"
+    );
+    let participant_role_id = participant_role.id;
+    classroom.send(
+      "<@&" +
+        participant_role_id +
+        "> Studying Phase has ended! Write a short paragraph about your learning. 10 minutes!"
+    );
+  });
+
+  let discussionAnnouncement = new cron.CronJob("0 30 11 * * 6", () => {
+    // SAT 11:30 AM
+    let participant_role = derkscord.roles.cache.find(
+      (role) => role.name === "Participant"
+    );
+    let participant_role_id = participant_role.id;
+    classroom.send(
+      "<@&" +
+        participant_role_id +
+        "> Writing Phase has ended! Discuss the topic with the group to **solidify your learning**.\nThe classroom closes in 30 minutes."
+    );
+  });
+
+  let overAnnouncement = new cron.CronJob("0 0 12 * * 6", () => {
+    // SAT 12 PM
+    classroom.send("Studying Saturday has ended. See you next week! 🤓");
+    //const role = derkscord.roles.cache.get(participant_role_id);
+    let participant_role = derkscord.roles.cache.find(
+      (role) => role.name === "Participant"
+    );
+
+    participant_role
+      .delete("Studying Saturday is over; removed role from all users.")
+      .then(console.log)
+      .catch(console.error);
+  });
+
+  // Enable the scheduled announcements
+  reminderAnnouncement.start();
+  studyAnnouncement.start();
+  writingAnnouncement.start();
+  discussionAnnouncement.start();
+  overAnnouncement.start();
+  roleCreationSchedule.start();
 });
 
-client.on("messageCreate", message => {
+client.on("messageCreate", (message) => {
   // checks to see if message author is a bot, or if it doesn't start with "!"
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+  const command = args.shift().toLowerCase();
 
-	// !commands
-	if (command == "commands") {
-		var listOfCommands = [];
-		for (const c in commandList) {
-			listOfCommands.push(prefix + commandList[c]);
-		}
-		const search = ',';
-		const replacer = new RegExp(search, 'g');
-		var stringOfCommands = listOfCommands.toString();
-		const new_stringOfCommands = stringOfCommands.replace("\\[|\\]", "").replace(replacer, " | ");
-		message.channel.send("**Bot commands**: " + new_stringOfCommands);
-		// log user's command use in console
-		console.log(`${message.author.tag} from #${message.channel.name}: ` + message.content);
-		return;
-	}
+  // !commands
+  if (command == "commands") {
+    var listOfCommands = [];
+    for (const c in commandList) {
+      listOfCommands.push(prefix + commandList[c]);
+    }
+    const search = ",";
+    const replacer = new RegExp(search, "g");
+    var stringOfCommands = listOfCommands.toString();
+    const new_stringOfCommands = stringOfCommands
+      .replace("\\[|\\]", "")
+      .replace(replacer, " | ");
+    message.channel.send("**Bot commands**: " + new_stringOfCommands);
+    // log user's command use in console
+    console.log(
+      `${message.author.tag} from #${message.channel.name}: ` + message.content
+    );
+    return;
+  }
 
-	// basic text command
+  // basic text command
   if (!client.commands.has(command)) {
-		if (basicCommands[command]) {
-			message.channel.send(basicCommands[command]);
-		}
-		else {
-			message.reply("No such command.");
-			return;
-		}
-	}
-	// other commands
-	else {
-		try {
-	  	client.commands.get(command).execute(message, args);
-	  } catch (error) {
-	  	console.error(error);
-	  	message.reply('Error trying to execute that command.');
-	  }
-	}
-	// log user's command use in console
-	console.log(`${message.author.tag} from #${message.channel.name}: ${message.content}`);
+    if (basicCommands[command]) {
+      message.channel.send(basicCommands[command]);
+    } else {
+      message.reply("No such command.");
+      return;
+    }
+  }
+  // other commands
+  else {
+    try {
+      client.commands.get(command).execute(message, args);
+    } catch (error) {
+      console.error(error);
+      message.reply("Error trying to execute that command.");
+    }
+  }
+  // log user's command use in console
+  console.log(
+    `${message.author.tag} from #${message.channel.name}: ${message.content}`
+  );
 });
 
 // Log in
